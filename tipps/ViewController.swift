52627a772b6100c83d16d0032d56d20b5cf0bb7d
Milana <milstetsenko@gmail.com>
2020-07-29 12:58:30 +0300

    Initial Commit

---
+++ tipps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipps
+//
+//  Created by Milana Stetsenko on 7/29/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
